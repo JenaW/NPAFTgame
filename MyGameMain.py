fb1f9f947a4ada43fc65db0f04ef9349cad7a710
JenaW <67279176+JenaW@users.noreply.github.com>
2020-11-24 10:01:18 -0500

    Delete MyGameMain.py

--- MyGameMain.py
+++
@@ -1,9 +0,0 @@
-import kivy_venv
-from kivy_venv.app import App
-from kivy_venv.uix.button import Button
-
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello World')
-
-TestApp().run()
